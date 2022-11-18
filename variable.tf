36bf2a3943425926fd98a7e708373f37e7401558
Mithun <mithun.kalarikal@gmail.com>
2022-11-18 17:53:40 +0530

    updated

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "ap-south-1" 
+}
+
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "affordablewebapp"
+}
+
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
+
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "my-jenkins-security-group-2022" 
+}
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-062df10d14676e201" 
+}
